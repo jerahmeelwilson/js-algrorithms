643449eac97f82acb37164162ce40c9d5ec46f78
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2023-03-24 22:02:28 -0500

    Updated freq counter

--- anagramFrequencyCounter.js
+++ anagramFrequencyCounter.js
@@ -18,34 +18,63 @@ validAnagram('texttwisttime', 'timetwisttext') // true
 */
 
 function validAnagram(str1, str2){
-    if(str1.length === 0 && str2.length ===0){
-        return true;
-    }
     if(str1.length !== str2.length){
         return false;
     }
-    let charCount1 = {};
-    let charCount2 = {};
+    let charCount = {};
+   
     for(let char of str1){
-        charCount1[char] = (charCount1[char] || 0) + 1;
+        charCount[char] = (charCount[char] || 0) + 1;
     }
-    for(let char of str2){
-        charCount2[char] = (charCount2[char] || 0) + 1;
-    }
-    for(key in charCount1){
-        if(charCount1[key] !== charCount2[key]){
-            return false;
+    for (let i = 0; i < str2.length; i++) {
+        let letter = str2[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!charCount[letter]) {
+          return false;
+        } else {
+          lookup[letter] -= 1;
         }
-    }
+      }
     return true;
 }
 
 
 
-console.log(validAnagram('', '')) // true
-console.log(validAnagram('aaz', 'zza')) // false
-console.log(validAnagram('anagram', 'nagaram')) // true
-console.log(validAnagram("rat","car")) // false
-console.log(validAnagram('awesome', 'awesom')) // false
-console.log(validAnagram('qwerty', 'qeywrt')) // true
-console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
+// console.log(validAnagram('', '')) // true
+// console.log(validAnagram('aaz', 'zza')) // false
+// console.log(validAnagram('anagram', 'nagaram')) // true
+// console.log(validAnagram("rat","car")) // false
+// console.log(validAnagram('awesome', 'awesom')) // false
+// console.log(validAnagram('qwerty', 'qeywrt')) // true
+// console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
+
+
+// function validAnagram(first, second) {
+//     if (first.length !== second.length) {
+//       return false;
+//     }
+  
+//     const lookup = {};
+  
+//     for (let i = 0; i < first.length; i++) {
+//       let letter = first[i];
+//       // if letter exists, increment, otherwise set to 1
+//       lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+//     }
+//     console.log(lookup)
+  
+//     for (let i = 0; i < second.length; i++) {
+//       let letter = second[i];
+//       // can't find letter or letter is zero then it's not an anagram
+//       if (!lookup[letter]) {
+//         return false;
+//       } else {
+//         lookup[letter] -= 1;
+//       }
+//     }
+  
+//     return true;
+//   }
+  
+//   // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+//   validAnagram('anagrams', 'nagaramm')
